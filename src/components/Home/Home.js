2525002d7cf2815752c622d1877f6210833bc2dd
loren pabst <lorenpabst@gmail.com>
2018-01-06 20:05:56 -0700

    flashcard4 done. Starting point for flashcard5

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component{
     render(){
         return(
-            <div>
-                home page
+            <div className='home'>
+                <div className='home_landing'>
+                    <div className='home_header'>
+                        <h2>Star Wars Trivia</h2>
+                        <p>Study up on your Star Wars trivia with our flashcards, then test yourself with one of our tests!</p>
+                    </div>
+                    <div className='home_header_btns'>
+                        <Link to='/study' className='home_header_btn'>Start Learning</Link>
+                        <Link to='/test' className='home_header_btn'>Test Me!</Link>
+                    </div>
+                </div>
             </div>
         );
     }
