7e2b39d924608d2041c927930e44b9c7f916bbdb
loren pabst <lorenpabst@gmail.com>
2018-01-10 22:19:26 -0700

    flashcard 4 done. starting point for flashcard 5

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component{
     render(){
         return(
-            <div>
-                home page
+            <div className='home' >
+                <div className='home_landing'>
+                    <div className='home_header'>
+                        <h2>Pokemon Trivia</h2>
+                        <p>Study up on your Pokemon knowledge, then test yourself with one of our sweet tests!</p>
+                    </div>
+                    <div className='home_header_btns'>
+                        <Link to='/study' className='home_header_btn' >Study Up</Link>
+                        <Link to='/test' className='home_header_btn' >Test Me!</Link>
+                    </div>
+                </div>
             </div>
         );
     }
